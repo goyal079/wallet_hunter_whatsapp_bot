097e5ac4038d4e70a289ece679947ac1574820da
goyal079 <himanshu@zoftwarehub.com>
2025-02-04 15:38:30 +0530

    database config updated

--- config/database.js
+++ config/database.js
@@ -6,34 +6,34 @@ const config = require("./db.config"); // Now referring to separate config file
 const env = process.env.NODE_ENV || "development";
 const dbConfig = config[env];
 let sequelize;
-// if (process.env.DB_URL) {
-//   // Production/Render configuration
-//   sequelize = new Sequelize(process.env.DB_URL, {
-//     dialect: "postgres",
-//     protocol: "postgres",
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     },
-//     logging: false,
-//   });
-// } else {
-// Local development configuration
-sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
-    host: dbConfig.host,
-    dialect: dbConfig.dialect,
-    pool: dbConfig.pool,
-    define: dbConfig.define,
-    logging: dbConfig.logging,
-  }
-);
-// }
+if (process.env.DB_URL) {
+  // Production/Render configuration
+  sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  });
+} else {
+  // Local development configuration
+  sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    {
+      host: dbConfig.host,
+      dialect: dbConfig.dialect,
+      pool: dbConfig.pool,
+      define: dbConfig.define,
+      logging: dbConfig.logging,
+    }
+  );
+}
 
 // Initialize db object
 const db = {};
